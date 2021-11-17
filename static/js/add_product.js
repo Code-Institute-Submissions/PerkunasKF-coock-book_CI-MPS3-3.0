84acfd77fbf9d5966ab1a5e669a709698d254135
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2021-11-17 13:06:27 +0000

    Add more coments for the add_product.js fiel

--- static/js/add_product.js
+++ static/js/add_product.js
@@ -24,33 +24,41 @@ function addProducts() {
 
   productList.appendChild(productDes);
 
+  // Creates clases and ids for li and host row
   productDes.setAttribute("class", "des-input-field");
   productDes.setAttribute("id", `product-list-item-${product}`);
   productDes.setAttribute("number", `${product}`);
   productDes.appendChild(productRow);
 
+  // Creats a row for input field and remuve button
   productRow.setAttribute("class", "row imput-container");
   productRow.appendChild(productImputCol);
   productRow.appendChild(remuveBtnCol);
   
+  // Creates container for input field
   productImputCol.setAttribute("class", "col s9");
   productImputCol.appendChild(productInput);
 
+  // Creates input field
   productInput.setAttribute("name", "product_description");
   productInput.setAttribute("type", "text");
   productInput.setAttribute("class", "recipe_ing");
   productInput.setAttribute("required", "");
 
+  // Creates remuve button
   remuveBtnCol.setAttribute("class", "col s3 remuve-btn");
   remuveBtnCol.setAttribute("id", `product-list-item-remuve-${product}`);
   remuveBtnCol.setAttribute("number", `${product}`);
   
+  // Moves text to remove button
   remuveBtnCol.appendChild(remuveBtnText);
   remuveBtnCol.appendChild(remuveBtnTextSmall);
 
+  // create text for big screens
   remuveBtnText.setAttribute("class", "remuve-btn-text center awarege-text big-catalog");
   remuveBtnText.appendChild(document.createTextNode("Remove"));
 
+  // Creates text for small screens
   remuveBtnTextSmall.setAttribute("class", "remuve-btn-text center awarege-text small-catalog");
   remuveBtnTextSmall.appendChild(document.createTextNode("X"));
 
@@ -69,6 +77,8 @@ function enterproduct () {
 }
 
 
+// Selects all of the list items generated with induvidual ids
+// Add remove function for all remuve buttons
 function removeProduct() {
   var removeBtns = document.querySelectorAll(".des-input-field");
   var num;
